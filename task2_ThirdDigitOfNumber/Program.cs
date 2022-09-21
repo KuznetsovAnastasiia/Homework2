05b53a4c71a0c2de7a0fd872e337d62ab90b001b
Kuznetsova Anastasiia <kuznetsova_nastasia@mail.ru>
2022-09-21 23:00:30 +0600

    Добавлено решение второго задания

---
+++ task2_ThirdDigitOfNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Напишите программу, которая выводит третью цифру 
+заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+string CreatNumber()
+{
+    Console.WriteLine("Введите число: ");
+    string number = Console.ReadLine();
+    return number;
+}
+
+string number = CreatNumber();
+
+void FindThirdDigit()
+{
+    int length = number.Length;
+    if (length > 2)
+    {
+        char ThirdDigit = number[2];
+        Console.WriteLine("Третья цифра заданного числа: " + ThirdDigit);
+    }
+    
+    if (length < 3)
+    {
+    Console.WriteLine("В заданном числе отсутствует третья цифра.");
+    }
+}
+
+FindThirdDigit();
